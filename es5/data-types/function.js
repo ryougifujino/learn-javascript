0ec027c803753d2401de0a7e7c70d1af1a092c14
ryougifujino <ryougifujino@gmail.com>
2019-01-24 15:46:02 +0800

    Add function.js

---
+++ es5/data-types/function.js
@@ -0,0 +1,23 @@
+// Function declaration
+var print = function p(x) {
+    if (typeof p === 'function') {
+        console.log(x);
+    }
+};
+
+var add = new Function('a', 'b', 'return a+b;');
+print(add(1, 1));
+
+// Properties and methods in function
+var printFunctionName = function (f) {
+    console.log(f.name);
+};
+var f = function myFun() {
+};
+
+function f2() {
+}
+
+printFunctionName(f);
+printFunctionName(printFunctionName);
+printFunctionName(f2);

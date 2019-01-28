225b2e7996388009c74f894427f9d11a28049329
ryougifujino <ryougifujino@gmail.com>
2019-01-28 11:19:18 +0800

    Add object.js

---
+++ es5/standard-library/object.js
@@ -0,0 +1,23 @@
+/** Instance method of Object */
+var type = function (o) {
+    return Object.prototype.toString.call(o).match(/\[object (.*?)]/)[1].toLowerCase();
+};
+
+['Null',
+    'Undefined',
+    'Object',
+    'Array',
+    'Number',
+    'Boolean',
+    'String',
+    'Function',
+    'RegExp'
+].forEach(function (t) {
+    type['is' + t] = function (o) {
+        return t.toLowerCase() === type(o);
+    };
+});
+
+console.log(type.isUndefined(undefined));
+console.log(type.isBoolean(true));
+

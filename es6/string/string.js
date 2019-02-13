e1add3b1c97707a4fe167e9a164f917d7406111b
ryougifujino <ryougifujino@gmail.com>
2019-02-13 22:41:57 +0800

    Add string

---
+++ es6/string/string.js
@@ -0,0 +1,12 @@
+/** includes(), startsWith(), endsWith() */
+let s = 'Good day~';
+console.log(s.includes('day'));
+console.log(s.startsWith('od', 2));
+console.log(s.endsWith('~'));
+
+/** repeat() */
+console.log('ok '.repeat(3));
+
+/** padStart()，padEnd() */
+console.log('1'.padStart(5, '0'));
+console.log('1'.padEnd(5, '-'));

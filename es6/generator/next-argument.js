2380ad303bcf725bf17e681dc35b5ff7184c250d
ryougifujino <ryougifujino@gmail.com>
2019-02-19 01:27:39 +0800

    Correct next-argument.js

--- es6/generator/next-argument.js
+++ es6/generator/next-argument.js
@@ -1,6 +1,7 @@
 const {log} = console;
 
 function* f() {
+    console.log('start');
     for (let i = 0; true; i++) {
         let reset = yield i;
         if (reset) i = -1;
@@ -12,14 +13,10 @@ log(g.next());
 log(g.next());
 log(g.next(true));
 // 注意next的参数是“上一个”yield表达式的返回值。
-// 第一个next调用value得到0，并停留在i等于1的yield处，此时next参数即使设置了也无效（因为没有上一个yield）；
-// 第二个next调用value得到1，因为有下一个next有参数true，所以yield 1这的reset被赋值为true，i被重置为-1。
-// 然后停留在i等于0的yield处。
-// 第三个next调用value得到0，并停留在i等于1的yield处。
-// 这是根据断点运行结果后的理解。
-
-// 或者这样理解：第一个next调用后执行到yield 0，value得到0，然后停下来；
-// 第二个next调用从上次的yield 0处继续执行，然后执行到yield 1处，value得到1，然后停下来；
-// 第三个next调用从上次的yield 1处继续执行，因为这次有参数true，所以reset的值变为true，i被重置为-1。然后
-// 进行下一个循环i加1变为0，执行到yield 0处，value得到0。
-// 这样理解起来更加自然。
+// 第一次调用next，从最开始（中途打印start）一直到首次循环中的yield 0处，此时返回value为0的对象并暂停；由于没有
+// 上一个yield语句，此时next中的参数（本例中为undefined）是何值都无任何作用。
+// 第二次调用next，从暂停的yield 0处开始执行，由于这次的参数也为undefined，所以 reset 为undefined，进行第二次
+// 循环，到yield 1处，此时返回value为1的对象并暂停。
+// 第三次调用next，从暂停的yield 1处开始执行，由于这次的参数为true，按照“next的参数是上一个yield表达式的返回值”
+// 这个原则，此时的 reset 的值变为true，继续执行，i变为-1；然后进行第三次循环，此时的i经过i++后变为0，然后停留
+// 在yield 0处，并返回value为0的对象并暂停。
